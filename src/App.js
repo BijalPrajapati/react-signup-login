5e97379975da6ff3767e25c67583386a5ee48d21
BijalPrajapati <bijalprajapati9898@gmail.com>
2024-09-24 22:03:23 +0200

    authentication

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user.username}</h1>
+          <h2>Email: {user.email}</h2>
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
